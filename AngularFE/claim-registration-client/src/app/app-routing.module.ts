2a593b3022502599bb1567dcc2d041702ed2cf38
efuente2 <earnestofuentes@gmail.com>
2022-12-29 16:20:08 -0500

    change to form

--- AngularFE/claim-registration-client/src/app/app-routing.module.ts
+++ AngularFE/claim-registration-client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClaimsComponent } from './claims/claims.component';
+import { SearchDeleteComponent } from './search-delete/search-delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "register", component: ClaimsComponent}
+  {path: "search", component: SearchDeleteComponent}
+  {path: "", redirectTo:"register", pathMatch:"full"}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
